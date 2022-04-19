3fcb4cf11b25002216c7d78ba77b59ede779bd5c
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-19 15:57:13 -0700

    added code to requires and listen sections.

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ console.log("ehlo");
 
 //  requires
 const data = require('./data/weather.json');
+const express = require('express'); //  imports express.js
+require('dotenv').config(); //  grabs config file
+const app = express();  //  defines and executes the app
+const PORT = process.env.PORT || 3002;  //  add fallback port
 
 //  use section - calls requires items
 
 //  routes - defines endpoints
 
 //  listen - starts the server
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 //  errors - define error handlers

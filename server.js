49236207770293b26d37e9508036293d79a721db
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-19 15:51:08 -0700

    added basic info and commented sections in server.js

--- server.js
+++ server.js
@@ -1,3 +1,15 @@
 'use strict';
 
+//  proof-of-life
 console.log("ehlo");
+
+//  requires
+const data = require('data/weather.json');
+
+//  use section - calls requires items
+
+//  routes - defines endpoints
+
+//  listen - starts the server
+
+//  errors - define error handlers

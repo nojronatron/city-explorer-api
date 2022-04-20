0f1415e4d9cd04c207536737987e8fe67fc4dec6
Jon Rumsey <Jon.Rumsey@gmail.com>
2022-04-19 21:49:12 -0700

    added cors due to cross site error, did not help, but will be needed later anyway.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //  proof-of-life
-console.log("ehlo");
+console.log('ehlo');
 
 //  requires
 const data = require('./data/weather.json');
@@ -9,6 +9,8 @@ const express = require('express'); //  imports express.js
 require('dotenv').config(); //  grabs config file
 const app = express();  //  defines and executes the app
 const PORT = process.env.PORT || 3002;  //  add fallback port
+const cors = require('cors');
+app.use(cors());
 
 //  classes
 class Forecast {
